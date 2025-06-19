b6f326f8e829ecbf194fcc3e73257eeb330560d7
DarleneOasu <kakosusvaimenaiskoristena@gmail.com>
2025-06-20 00:43:02 +0200

    dropdowni dodani na pretraživnje konflikata

--- src/main/java/com/example/projektjava/controller/ConflictSearchController.java
+++ src/main/java/com/example/projektjava/controller/ConflictSearchController.java
@@ -4,28 +4,32 @@ import com.example.projektjava.AlertScreen;
 import com.example.projektjava.AppConstants;
 import com.example.projektjava.UserSession;
 import com.example.projektjava.dataBase.DataBase;
+import com.example.projektjava.enums.StatusEnum;
 import com.example.projektjava.model.ConflictForm;
 import com.example.projektjava.model.User;
+import com.example.projektjava.model.UserPrint;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ConflictSearchController implements AlertScreen {
     @FXML
-    public ChoiceBox involved;
+    public ChoiceBox<String> involved;
     @FXML
-    public ChoiceBox reportedBy;
+    public ChoiceBox<String> reportedBy;
     @FXML
     public DatePicker date;
     @FXML
-    public ChoiceBox status;
+    public ChoiceBox<StatusEnum> status;
 
     @FXML
     private TableView<ConflictForm> conflictTableView;
@@ -61,14 +65,31 @@ public class ConflictSearchController implements AlertScreen {
                     return new SimpleStringProperty(names);
                 }
             );
-        //dodaj dropdowne
+        status.getItems().addAll(StatusEnum.values());
+        List<UserPrint> users = DataBase.getAllUsersPrint();
+        List<String> usersList = users.stream()
+                .map(u -> u.getName() + " (" + u.getId() + ")")
+                .toList();
+        involved.getItems().addAll(usersList);
+        reportedBy.getItems().addAll(usersList);
     }
 
     @FXML
     protected void search() {
+        List<ConflictForm> allConflicts = DataBase.getAllConflicts();
+        List<ConflictForm> matchedConflicts = new ArrayList<>();
+
+
+
+        conflictTableView.setItems(FXCollections.observableArrayList(matchedConflicts));
+
     }
 
     @FXML
     protected void clear() {
+        status.setValue(null);
+        date.setValue(null);
+        involved.setValue(null);
+        reportedBy.setValue(null);
     }
 }
